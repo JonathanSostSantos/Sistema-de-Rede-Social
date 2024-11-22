23e014c0d1aafa3db72a5c0f05658b49ea6470e3
Jonathan Sost <jonathan_santos11@estudante.sesisenai.org.br>
2024-11-21 22:40:25 -0300

    Implementados os métodos exibirMenu e criarPost

--- src/ui/MenuUsuario.java
+++ src/ui/MenuUsuario.java
@@ -1,12 +1,75 @@
 package ui;
 
-public class MenuUsuario {
-    public void exibirMenu() {
+import gerenciador.GerenciadorPosts;
+import modelo.Post;
 
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class MenuUsuario {
+    private MenuPrincipal menu;
+    private Scanner leitor;
+
+    public void exibirMenu() {
+        Integer opcaoSelecionada;
+        menu = new MenuPrincipal();
+        System.out.println(ConsoleColors.CYAN_BOLD_BRIGHT + "Bem-vindo(a) de volta, " + ConsoleColors.GREEN_BOLD_BRIGHT + menu.getUsuarioLogado().getNome() + ConsoleColors.CYAN_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
+        while(true) {
+            System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "\n\nO que você deseja fazer?" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "\n1- Criar post");
+            System.out.println("2- Ver meu perfil");
+            System.out.println("3- Buscar usuários");
+            System.out.println("4- Gerenciar amizades");
+            System.out.println("5- Ver feed de notícias");
+            System.out.println("6- Logout" + ConsoleColors.RESET);
+            opcaoSelecionada = menu.validarEntradaInteira(leitor.nextLine().trim());
+
+            if (opcaoSelecionada != null) {
+                switch(opcaoSelecionada) {
+                    case 1:
+                        criarPost();
+                        break;
+                    case 2:
+                        verPerfil();
+                        break;
+                    case 3:
+                        buscarUsuarios();
+                        break;
+                    case 4:
+                        gerenciarAmizades();
+                        break;
+                    case 5:
+                        verFeedNoticias();
+                        break;
+                    case 6:
+                        System.out.println(ConsoleColors.YELLOW_UNDERLINED + "Logout realizado!" + ConsoleColors.RESET);
+                        return;
+                    default:
+                        System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Opção inexistente." + ConsoleColors.RESET);
+                }
+            }
+        }
     }
 
     private void criarPost() {
+        String valorInserido;
+        GerenciadorPosts gerenciador = new GerenciadorPosts();
+        while (true) {
+            System.out.print(ConsoleColors.BLUE_BOLD_BRIGHT + "Digite o que você está pensando (máximo 255 caracteres): " + ConsoleColors.RESET);
+            valorInserido = leitor.nextLine();
 
+            if (menu.getUsuarioLogado() == null) {
+                System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Houve um erro com a autenticação de login. Retornando ao menu." + ConsoleColors.RESET);
+                return;
+            } else if (valorInserido.length() > 255) {
+                System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "O limite é de 255 caracteres por post." + ConsoleColors.RESET);
+            } else if (valorInserido.length() <= 0) {
+                System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "O post não pode estar vazio." + ConsoleColors.RESET);
+            }
+
+            gerenciador.criar(new Post(menu.getUsuarioLogado(), valorInserido, LocalDateTime.now()));
+            System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Post publicado com sucesso!" + ConsoleColors.RESET);
+        }
     }
 
     private void verPerfil() {
