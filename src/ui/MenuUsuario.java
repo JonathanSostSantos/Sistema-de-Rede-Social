469e8243423cbba61591133125a160e79d8b4e7b
Jonathan Sost <jonathan_santos11@estudante.sesisenai.org.br>
2024-11-22 12:41:46 -0300

    Implementado o método verPosts

--- src/ui/MenuUsuario.java
+++ src/ui/MenuUsuario.java
@@ -24,7 +24,7 @@ public class MenuUsuario {
     public void exibirMenu() {
         Integer opcaoSelecionada;
         System.out.println(ConsoleColors.CYAN_BOLD_BRIGHT + "Bem-vindo(a) de volta, " + ConsoleColors.GREEN_BOLD_BRIGHT + usuarioLogado.getNome() + ConsoleColors.CYAN_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
-        while(true) {
+        while (true) {
             System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "\n\nO que você deseja fazer?" + ConsoleColors.RESET);
             System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "\n1- Criar post");
             System.out.println("2- Ver meu perfil");
@@ -35,7 +35,7 @@ public class MenuUsuario {
             opcaoSelecionada = menu.validarEntradaInteira(leitor.nextLine().trim());
 
             if (opcaoSelecionada != null) {
-                switch(opcaoSelecionada) {
+                switch (opcaoSelecionada) {
                     case 1:
                         criarPost();
                         break;
@@ -68,6 +68,8 @@ public class MenuUsuario {
             System.out.print(ConsoleColors.BLUE_BOLD_BRIGHT + "Digite o que você está pensando (máximo 255 caracteres): " + ConsoleColors.RESET);
             valorInserido = leitor.nextLine();
 
+            if (valorInserido.isBlank()) break;
+
             if (usuarioLogado == null) {
                 System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Houve um erro com a autenticação de login. Retornando ao menu." + ConsoleColors.RESET);
                 return;
@@ -75,10 +77,11 @@ public class MenuUsuario {
                 System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "O limite é de 255 caracteres por post." + ConsoleColors.RESET);
             } else if (valorInserido.length() <= 0) {
                 System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "O post não pode estar vazio." + ConsoleColors.RESET);
+            } else {
+                gerenciador.criar(new Post(usuarioLogado, valorInserido, LocalDateTime.now()));
+                System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Post publicado com sucesso!" + ConsoleColors.RESET);
+                break;
             }
-
-            gerenciador.criar(new Post(usuarioLogado, valorInserido, LocalDateTime.now()));
-            System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Post publicado com sucesso!" + ConsoleColors.RESET);
         }
     }
 
@@ -114,7 +117,44 @@ public class MenuUsuario {
     }
 
     private void verPosts() {
+        Integer pagina = 0;
+        Integer totalPosts = usuarioLogado.getPosts().size();
+        Integer totalPaginas;
+        String valorInserido;
 
+        if (totalPosts <= 0) {
+            System.out.println("Você não possui nenhum post, que tal fazer seu primeiro?");
+        } else {
+            totalPaginas = totalPosts / 10;
+            System.out.println("Seus posts:");
+
+            while (true) {
+                for (int i = pagina * 10; i < (pagina * 10) + 10; i++) {
+                    System.out.println(usuarioLogado.getPosts().get(i));
+                }
+                System.out.println("\n" + (pagina > 0 ? pagina > 1 ? "<< <" : "<" : "") + " Página " + (pagina + 1) + " de " + (totalPaginas + 1) + (pagina < totalPaginas ? pagina < totalPaginas - 1 ? " >> >" : " >" : ""));
+                valorInserido = leitor.nextLine();
+                switch (valorInserido) {
+                    case "<<":
+                        if (pagina > 1) pagina = 0;
+                        break;
+                    case "<":
+                        if (pagina > 0) pagina--;
+                        break;
+                    case ">":
+                        if (pagina < totalPaginas) pagina++;
+                        break;
+                    case ">>":
+                        if (pagina < (totalPaginas - 1)) pagina = totalPaginas;
+                        break;
+                    default:
+                        if (valorInserido.matches(".*[0-9].*")) {
+
+                        }
+                        break;
+                }
+            }
+        }
     }
 
     private void editarPerfil() {
