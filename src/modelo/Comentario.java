46982538a8366f04595de8a9f1ec9fda411c782d
Jonathan Sost <jonathan_santos11@estudante.sesisenai.org.br>
2024-11-23 21:40:22 -0300

    Adicionados comentários na classe Comentario

--- src/modelo/Comentario.java
+++ src/modelo/Comentario.java
@@ -5,6 +5,12 @@ import ui.ConsoleColors;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Representa um comentário na rede social.
+ * Classe responsável por manter as informações de um comentário individual no sistema.
+ *
+ * @author Jonathan Sost Dos Santos
+ */
 public class Comentario {
     private Integer id;
     private Usuario autor;
@@ -13,6 +19,14 @@ public class Comentario {
     private Post post;
     private Integer proximoId = 1;
 
+    /**
+     * Construtor para criar um novo comentário sem passar ID como parâmetro.
+     * O ID será atribuído utilizando o atributo local proximoId.
+     *
+     * @param autor Autor do comentário
+     * @param conteudo Conteúdo do comentário - não pode ser vazio
+     * @param post O post onde o comentário está sendo publicado
+     */
     public Comentario(Usuario autor, String conteudo, Post post) {
         this.id = proximoId;
         this.autor = autor;
@@ -22,6 +36,11 @@ public class Comentario {
         proximoId++;
     }
 
+    /**
+     * Retorna uma representação em String do comentário já formatado.
+     *
+     * @return String formatada com todos os dados do comentário que serão apresentados ao usuário
+     */
     @Override
     public String toString() {
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
